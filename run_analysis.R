de337960475a9c58f0ee1616fe626ebdaea7be2b
ohaganb <barry.ohagan2@merck.com>
2014-09-20 12:20:05 -0400

    added code for tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -22,8 +22,8 @@ colnames(ds_2_cols) <- "Labels"
                                                             # adds a label for the column
 
 
-colnames(proj_ds_2, do.NULL =TRUE)
-colnames(proj_ds_2[,1]) <- ds_2_cols[1,]
+#colnames(proj_ds_2, do.NULL =TRUE)
+#colnames(proj_ds_2[,1]) <- ds_2_cols[1,]
 
 
 proj_ds_3 <- read.table("C:/Users/ohaganb/Desktop/BI and Analytics/Cloudera/GetCleanData/project/UCI HAR Dataset/train/y_train.txt", header=FALSE, sep="")
@@ -42,6 +42,8 @@ proj_ds_all <- cbind(proj_ds_all, proj_ds_3)
                                                             # combines the 
                                                             # third txt file with the 
                                                             # first and second txt files
+tidy <- summary(proj_ds_all)
 
-head(proj_ds_all)
+#head(proj_ds_all)
 
+write.table(tidy, "C:/Users/ohaganb/Desktop/BI and Analytics/Cloudera/GetCleanData/project/tidy.txt", row.name=FALSE)
